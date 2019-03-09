da2c29e58d04f24d83e2a988e5949d38e4866bbc
busradogann <30301977+busradogann@users.noreply.github.com>
2019-03-10 02:26:17 +0300

    Update urls.py

--- hr/urls.py
+++ hr/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('manager/create', ManagerJobCreate.as_view(), name='manager_job_create'),
     path('manager/applications', ManagerApplicationList.as_view(), name='manager_application_list'),
     path('manager/applications/<int:pk>', ManagerApplicationDetail.as_view(), name='manager_application_detail'),
-	path('signup/', signup, name='signup'),
+    path('signup/', signup, name='signup'),
     path('', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
